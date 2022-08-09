17247ad38da82cdf3f200fb787a84db1e781597c
Mohamed Hamdy <m.hamdy@cequens.com>
2022-08-09 23:11:14 +0200

    App UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.scss";
 
 function App() {
-
+  const [userKey, setUserKey] = useState("");
+  const [keyPAT, setKeyPAT] = useState("");
+  const [commitsList, setCommitsList] = useState<any>([]);
+  const [isInputDisabled, setIsInputDisabled] = useState(false);
+  useEffect(() => {
+    const userKeyFromLocal = localStorage.getItem("userKey");
+    if (userKeyFromLocal) {
+      setUserKey(userKeyFromLocal);
+      setKeyPAT(userKeyFromLocal);
+      setIsInputDisabled(true);
+    }
+  });
+  const handleChange = (key: string) => {
+    setUserKey(key);
+    console.log(userKey);
+  };
+  const insertKey = () => {
+    localStorage.setItem("userKey", userKey);
+    setKeyPAT(userKey);
+    setIsInputDisabled(true);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div className="keyForm">
+        <input
+          name="key"
+          type="text"
+          value={userKey}
+          disabled={isInputDisabled}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+        <button className="actionBtn insertBtn" onClick={insertKey}>
+          Insert
+        </button>
+        <button
+          className="actionBtn resetBtn"
+          onClick={() => {
+            localStorage.removeItem("userKey");
+            setUserKey("");
+            setIsInputDisabled(false);
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          Reset
+        </button>
+      </div>
+      <ul className="commitsList">
+        {commitsList?.map((item : any) => (
+          <li key={item.node_id} className="commitItem">
+            <label>{item.message}</label>
+            <div>
+              <span className="commitTime">{item.author.date}</span>
+              <span className="commitAuther">{item.author.name}</span>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
